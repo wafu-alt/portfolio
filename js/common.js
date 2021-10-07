8603d01c1d22897884161490f3cd51b1fe6b985e
wafu-alt <rhfemtlfem@naver.com>
2021-10-07 13:46:33 +0900

    업데이트

--- js/common.js
+++ js/common.js
@@ -1,13 +1,54 @@
 $(document).ready(function () {
-    new fullpage('#fullpage', {
-        responsiveWidth: 1024,
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        parallax: true,
-        autoScrolling: false,
-        scrollOverflow: true,
-        sectionsColor: ['#ccc', '#f0f', '#ff0', '#fff', '#ddd'],
-        onLeave: function(origin, destination, direction) {
-            console.log("Leaving section" + origin.index);
-        },
+
+    //active 클래스 추가
+
+    // $('.gnb a').on('click', function(event) {
+    //     $(this).parents().find('li a').removeClass('active');
+    //     $(this).addClass('active');
+    // });
+    
+    // $(window).on('scroll', function() {
+    //     $('.target').each(function() {
+    //         if($(window).scrollTop() >= $(this).offset().top) {
+    //             var id = $(this).attr('id');
+    //             $('.gnb li.a').removeClass('active');
+    //             $('.gnb li.a[href=#'+ id +']').addClass('active');
+    //         }
+    //     });
+    // });
+    
+    
+    //current 클래스 추가
+    $('.AboutME').click(function () { 
+        $(this).addClass('current')
+        $('.Skills').removeClass('current');
     });
+    $('.Skills').click(function () { 
+        $(this).addClass('current')
+        $('.AboutME').removeClass('current');
+    });
+    
+    
+    
+    //작품 슬라이드
+    $('.mv').slick({ 
+        autoplay:true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots:true,
+        infinite: true,
+        cssEase: 'linear',
+        speed: 300,
+        fade: true
+    });
+
+
+    //작품 슬라이드
+    $('.works').slick({ 
+        autoplay:false,
+        dots:true,
+        fade: false
+    });
+
+
 });
